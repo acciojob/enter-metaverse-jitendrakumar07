1e0ac59def1a536b9a7ebb11beedfd35433093f2
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-03-16 23:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const statusParagraph = document.getElementById("status");
+    const enterButton = document.getElementById("enterBtn");
+
+    enterButton.addEventListener("click", () => {
+      statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>"; 
+    });
